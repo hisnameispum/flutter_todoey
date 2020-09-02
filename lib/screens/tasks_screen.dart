c9f88ab92707a903cb6384a124df406571777ccb
hisnameispum <sirasitxp@gmail.com>
2020-09-02 02:54:40 -0700

    Added top part of the design

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,7 +1,46 @@
 import 'package:flutter/material.dart';
+
 class TasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: Container(
+
+        padding: EdgeInsets.only(top: 60, left: 30, right: 30, bottom: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CircleAvatar(
+              backgroundColor: Colors.white,
+              radius: 30,
+              child: Icon(
+                Icons.list,
+                color: Colors.lightBlueAccent,
+                size: 30,
+              ),
+            ),
+            SizedBox(height: 10,),
+            Text(
+              "Todoey",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 50,
+                fontWeight: FontWeight.w700
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text('12 tasks',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18
+            ),),
+
+          ],
+        ),
+      ),
+    );
   }
 }
