d1001e0d64bf37e527211d647e678a2eb4ae74b3
hisnameispum <sirasitxp@gmail.com>
2020-09-02 15:44:11 -0700

    Added Information inside the bottomsheet

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -1,7 +1,42 @@
 import 'package:flutter/material.dart';
+
 class AddTaskScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Color(0xff757575),
+      child: Container(
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20), topRight: Radius.circular(20))),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+
+            Text(
+              'Add Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.lightBlueAccent,
+              ),
+            ),
+            TextField(
+              autofocus: true,
+            ),
+            FlatButton(
+              child: Text('Add'),
+              color: Colors.lightBlueAccent,
+              textColor: Colors.white,
+              onPressed: () {
+                //Add new task to to-do list
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
