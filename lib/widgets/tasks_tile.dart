62d038d5d81f27199d7c0ba9ae92cc2db3326657
hisnameispum <sirasitxp@gmail.com>
2020-09-10 16:17:20 -0700

    Added checkbox functionality

--- lib/widgets/tasks_tile.dart
+++ lib/widgets/tasks_tile.dart
@@ -5,10 +5,29 @@ class TasksTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('Workout'),
-      trailing: Checkbox(
-        value: false,
-        activeColor: Colors.lightBlueAccent,
-      ),
+      trailing: CustomCheckBox(),
+    );
+  }
+}
+
+class CustomCheckBox extends StatefulWidget {
+  @override
+  _CustomCheckBoxState createState() => _CustomCheckBoxState();
+}
+
+class _CustomCheckBoxState extends State<CustomCheckBox> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: isChecked,
+      activeColor: Colors.lightBlueAccent,
+      onChanged: (newValue){
+        setState(() {
+          isChecked = newValue;
+        });
+      },
     );
   }
 }
