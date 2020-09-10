d4e4703cb39ee78e8c0f5bd8b93f47af7defb329
hisnameispum <sirasitxp@gmail.com>
2020-09-10 16:09:19 -0700

    Modified Checkbox

--- lib/widgets/tasks_tile.dart
+++ lib/widgets/tasks_tile.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
 class TasksTile extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('Workout'),
-      trailing: Checkbox(value:false),
+      trailing: Checkbox(
+        value: false,
+        activeColor: Colors.lightBlueAccent,
+      ),
     );
   }
 }
+
