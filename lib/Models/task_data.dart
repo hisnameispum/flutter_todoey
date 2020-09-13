68c43fdde7989d2e515ac8f68b59f203b5ac5961
hisnameispum <sirasitxp@gmail.com>
2020-09-13 02:55:47 -0700

    Added getCount() and addTask() methods

--- lib/Models/task_data.dart
+++ lib/Models/task_data.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_todoey/Models/task.dart';
+import 'package:provider/provider.dart';
 class TaskData extends ChangeNotifier{
   List<Task> tasks = [
     Task(taskName: 'Read book'),
     Task(taskName: 'Eat good food'),
     Task(taskName: 'Become a millionaire'),
   ];
+
+  int get taskCount {
+    return tasks.length;
+  }
+
+  void addTask(String newTaskTitle){
+    final task = Task(newTaskTitle);
+    tasks.add(task);
+    notifyListeners();
+  }
 }
